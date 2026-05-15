9011e35d821d8a945e4447b6bbcf3604326325e8
Wilfredo <wifredo.gonzalez2019@gmail.com>
2026-05-15 10:13:54 -0600

    Endpoints_ventas

--- main.py
+++ main.py
@@ -105,3 +105,51 @@ def eliminar_cliente(cliente_id: str):
         return {"message": "Cliente eliminado exitosamente"}
     else:
         return {"message": "Cliente no encontrado"}, 404
+
+@api.get("/ventas")
+def listar_ventas():
+    """Endpoint para listar las ventas"""
+    database = load_database()
+    return database
+
+@api.get("/ventas/{venta_id}")
+def obtener_ventas(venta_id: str):
+    """Endpoint para obtener una venta por su ID"""
+    database = load_database()
+    venta = database.get(venta_id)
+    if venta:
+        return venta
+    else:
+        return {"message": "Venta no encontrada"}, 404
+    
+@api.post("/ventas")
+def agregar_venta(venta: Venta):
+    """Endpoint para agregar una nueva venta a la base de datos"""
+    database = load_database()
+    venta_id = str(len(database) + 1)  # Genera un ID simple basado en el número de ventas
+    database[venta_id] = venta
+    guardar_database(database)
+    return {"message": "Venta agregada exitosamente", "venta_id": venta_id}
+
+@api.put("/ventas/{venta_id}")
+def actualizar_venta(venta_id: str, venta: Venta):
+    """Endpoint para actualizar una venta existente"""
+    database = load_database()
+    if venta_id in database:
+        database[venta_id] = venta
+        guardar_database(database)
+        return {"message": "Venta actualizada exitosamente"}
+    else:
+        return {"message": "Venta no encontrada"}, 404
+    
+@api.delete("/ventas/{venta_id}")
+def eliminar_venta(venta_id: str):
+    """Endpoint para eliminar una venta por su ID"""
+    database = load_database()
+    if venta_id in database:
+        del database[venta_id]
+        guardar_database(database)
+        return {"message": "Venta eliminada exitosamente"}
+    else:
+        return {"message": "Venta no encontrada"}, 404
+    
