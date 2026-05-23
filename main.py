7c901af4f47551cce7060b92a8b769d2e4840b34
Luis Mendez <lamendeza@alumno.uspg.edu.gt>
2026-05-23 14:51:32 -0600

    ahora si

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from endpoints.clientes import clientes_router
 from endpoints.productos import productos_router
 from endpoints.abonos import abonos_router
 from endpoints.ventas import ventas_router
+from endpoints.producto_venta import producto_venta_router
 
 JSON_DATABASE_FILE = "database.json"
 app = FastAPI()
@@ -15,3 +16,4 @@ app.include_router(clientes_router)
 app.include_router(productos_router)
 app.include_router(abonos_router)
 app.include_router(ventas_router)
+app.include_router(producto_venta_router)
